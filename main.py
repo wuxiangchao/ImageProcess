4a34087d3ab819c0ab7752774f846f510d5f1200
WUXIANGCHAO <709894549@qq.com>
2021-02-24 14:13:08 +0800

    mod main.py

--- main.py
+++ main.py
@@ -3,18 +3,12 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import numpy as np
+import matplotlib.pyplot as plt
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print("Test git")
+x = np.linspace(0, 10 * np.pi, 500)
 
+y = np.sin(x)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
-#add
+plt.plot(x, y)
+plt.show()
